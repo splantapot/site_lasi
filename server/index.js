43a4dd28a5d8ef65419d7a7e2b8bac0d3d08faa8
splantapot <joaovictor1007@outlook.com.br>
2026-05-17 12:06:23 -0300

    Testing files download

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzGfTkQ6xI-aSQUAGB-
 const TIMEOUT = 10000; // 10 seconds
 let data = [];
 
-const x = 1000;
-
 await axios.get(WEB_APP_URL, {timeout: TIMEOUT})
   .then((response) => {
     data = response.data;
